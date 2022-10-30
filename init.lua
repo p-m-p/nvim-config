91ac38e90790c8b277501c01f4081a4237c0d9ae
Phil Parsons <p-m-p@users.noreply.github.com>
2022-10-30 09:06:47 +0000

    feat: add gutter icons and use lua settings

--- init.lua
+++ init.lua
@@ -1,11 +1,24 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.g.mapleader = ";"
 
-vim.cmd([[
-  set number
+vim.o.number = true
 
-  nnoremap <silent> ca <cmd>lua vim.lsp.buf.code_action()<CR>
-  nnoremap <leader>c :noh<cr>
-]])
+vim.api.nvim_set_keymap("n", "<leader>c", ":noh<cr>", {
+  silent = true
+})
+vim.api.nvim_set_keymap("n", "ca", "", {
+  silent = true,
+  callback = function ()
+    vim.lsp.buf.code_action()
+  end
+})
+
+-- Diagnostic gutter icons
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
 
 require("plugins")
