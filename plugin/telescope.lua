431506ddef27b40527cdf77c4f12798c6d9fb4eb
Phil Parsons <p-m-p@users.noreply.github.com>
2022-11-09 13:56:11 +0000

    feat: add key maps for telescope

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -11,3 +11,6 @@ local builtin = require('telescope.builtin')
 local bufopts = { noremap = true, silent = true }
 vim.keymap.set("n", "<leader>d", function() builtin.lsp_type_definitions() end, bufopts)
 vim.keymap.set("n", "<leader>r", function() builtin.lsp_references() end, bufopts)
+vim.keymap.set("n", "<leader>/", function() builtin.live_grep() end, bufopts)
+vim.keymap.set("n", "<leader>*", function() builtin.grep_string() end, bufopts)
+vim.keymap.set("n", "<leader>\"", function() builtin.registers() end, bufopts)
