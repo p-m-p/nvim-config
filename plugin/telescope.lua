d7a6f64218e684e3467ce055856baa884f52eb53
Phil Parsons <p-m-p@users.noreply.github.com>
2022-11-09 14:07:51 +0000

    feat: add telescope git key mappings

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -14,3 +14,8 @@ vim.keymap.set("n", "<leader>r", function() builtin.lsp_references() end, bufopt
 vim.keymap.set("n", "<leader>/", function() builtin.live_grep() end, bufopts)
 vim.keymap.set("n", "<leader>*", function() builtin.grep_string() end, bufopts)
 vim.keymap.set("n", "<leader>\"", function() builtin.registers() end, bufopts)
+vim.keymap.set("n", "<leader>gs", function() builtin.git_status() end, bufopts)
+vim.keymap.set("n", "<leader>gt", function() builtin.git_stash() end, bufopts)
+vim.keymap.set("n", "<leader>gb", function() builtin.git_branches() end, bufopts)
+vim.keymap.set("n", "<leader>gc", function() builtin.git_commits() end, bufopts)
+vim.keymap.set("n", "<leader>gbc", function() builtin.git_bcommits() end, bufopts)
