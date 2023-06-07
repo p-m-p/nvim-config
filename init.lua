02465fb3a4cde0d561d8510da1f3837e380a6c05
Phil Parsons <p-m-p@users.noreply.github.com>
2023-06-07 20:46:09 +0100

    fix: update missing icons

--- init.lua
+++ init.lua
@@ -16,7 +16,7 @@ vim.api.nvim_set_keymap("n", "ca", "", {
 })
 
 -- Diagnostic gutter icons
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = "", Warn = "", Hint = "", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
