ee284857220ee80597c4cedd341aea8e744889df
Phil Parsons <p-m-p@users.noreply.github.com>
2025-04-04 17:34:40 +0100

    chore: tidy plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,11 +1,13 @@
 local ensure_packer = function()
   local fn = vim.fn
   local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+
   if fn.empty(fn.glob(install_path)) > 0 then
     fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
     vim.cmd [[packadd packer.nvim]]
     return true
   end
+
   return false
 end
 
@@ -13,31 +15,47 @@ local packer_bootstrap = ensure_packer()
 
 return require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
-
-  -- Support
-  use "nvim-tree/nvim-web-devicons"
-
-  -- Theme
-  use {
-    "catppuccin/nvim",
-    as = "catppuccin",
-    config = function()
-      require("catppuccin").setup {
-        flavour = "mocha", -- mocha, macchiato, frappe, latte
-      }
-      vim.api.nvim_command "colorscheme catppuccin"
-    end,
-  }
+  use "nvim-lua/plenary.nvim"
 
   -- Editor
   use {
+    "nvim-tree/nvim-web-devicons",
     "nvim-lualine/lualine.nvim",
     "nvim-tree/nvim-tree.lua",
+    {
+      "catppuccin/nvim",
+      as = "catppuccin",
+      config = function()
+        require("catppuccin").setup { flavour = "mocha" }
+        vim.api.nvim_command "colorscheme catppuccin"
+      end,
+    },
+    {
+      "editorconfig/editorconfig-vim",
+      setup = function()
+        vim.g.EditorConfig_max_line_indicator = "none"
+        vim.g.EditorConfig_preserve_formatoptions = 1
+      end,
+    },
+    "jiangmiao/auto-pairs",
+    "kshenoy/vim-signature",
+    {
+      "nvim-telescope/telescope.nvim",
+      tag = "0.1.8",
+    },
+    "nvim-telescope/telescope-ui-select.nvim",
+    {
+      "nvim-treesitter/nvim-treesitter",
+      run = ":TSUpdate",
+    },
   }
 
   -- LSP
   use {
-    { "williamboman/mason.nvim", run = ":MasonUpdate" },
+    {
+      "williamboman/mason.nvim",
+      run = ":MasonUpdate",
+    },
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
     "nvimtools/none-ls.nvim",
@@ -46,23 +64,8 @@ return require("packer").startup(function(use)
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
     "hrsh7th/nvim-cmp",
-    { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
     "b0o/SchemaStore.nvim",
     "mfussenegger/nvim-jdtls",
-  }
-
-  -- Code formatting
-  use {
-    "tpope/vim-surround",
-    "tpope/vim-sleuth",
-    "jiangmiao/auto-pairs",
-    {
-      "editorconfig/editorconfig-vim",
-      setup = function()
-        vim.g.EditorConfig_max_line_indicator = "none"
-        vim.g.EditorConfig_preserve_formatoptions = 1
-      end,
-    },
   }
 
   -- Git
@@ -70,19 +73,6 @@ return require("packer").startup(function(use)
     "tpope/vim-fugitive",
     "tpope/vim-rhubarb",
   }
-
-  -- Marks
-  use "kshenoy/vim-signature"
-
-  -- Search
-  use {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.3",
-    requires = {
-      "nvim-lua/plenary.nvim",
-    },
-  }
-  use "nvim-telescope/telescope-ui-select.nvim"
 
   -- AI
   use "github/copilot.vim"
