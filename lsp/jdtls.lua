bce3028d11ff7debde4aef7dd8517b9a60d14083
Phil Parsons <p-m-p@users.noreply.github.com>
2025-05-27 09:03:05 +0100

    refactor: move jdtls config to lsp config file

--- lsp/jdtls.lua
+++ lsp/jdtls.lua
@@ -1,11 +1,4 @@
-local ok, jdtls = pcall(require, "jdtls")
-
-if not ok then
-  return
-end
-
-local config = {
+return {
   cmd = { vim.fn.stdpath "data" .. "/mason/bin/jdtls" },
   root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
 }
-jdtls.start_or_attach(config)
