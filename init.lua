07159ca7deaa5f4ee808296d6583b99f516c47bf
Phil Parsons <p-m-p@users.noreply.github.com>
2025-12-01 11:27:40 +0000

    refactor: migrate plugins to init.lua

--- init.lua
+++ init.lua
@@ -51,8 +51,47 @@ vim.api.nvim_create_autocmd("LspAttach", {
   end,
 })
 
--- Load plugins with vim-plug
-vim.cmd("source " .. vim.fn.stdpath("config") .. "/plugins.vim")
+-- Plugins via vim-plug (https://github.com/junegunn/vim-plug)
+local Plug = vim.fn["plug#"]
 
--- Set colorscheme after plugins are loaded
-pcall(vim.cmd.colorscheme, "catppuccin-mocha")
+vim.call "plug#begin"
+
+-- Core dependencies
+Plug "nvim-lua/plenary.nvim"
+
+-- Editor
+Plug "nvim-tree/nvim-web-devicons"
+Plug "nvim-lualine/lualine.nvim"
+Plug "nvim-tree/nvim-tree.lua"
+Plug("catppuccin/nvim", { as = "catppuccin" })
+Plug "editorconfig/editorconfig-vim"
+Plug "jiangmiao/auto-pairs"
+Plug "kshenoy/vim-signature"
+Plug("nvim-telescope/telescope.nvim", { tag = "v0.2.0" })
+Plug "nvim-telescope/telescope-ui-select.nvim"
+Plug("nvim-treesitter/nvim-treesitter", { ["do"] = ":TSUpdate" })
+
+-- LSP
+Plug("williamboman/mason.nvim", { ["do"] = ":MasonUpdate" })
+Plug "williamboman/mason-lspconfig.nvim"
+Plug "neovim/nvim-lspconfig"
+Plug "nvimtools/none-ls.nvim"
+Plug "hrsh7th/cmp-nvim-lsp"
+Plug "hrsh7th/cmp-buffer"
+Plug "hrsh7th/cmp-path"
+Plug "hrsh7th/cmp-cmdline"
+Plug "hrsh7th/nvim-cmp"
+Plug "b0o/SchemaStore.nvim"
+Plug "mfussenegger/nvim-jdtls"
+
+-- Git
+Plug "tpope/vim-fugitive"
+Plug "tpope/vim-rhubarb"
+
+-- AI
+Plug "github/copilot.vim"
+Plug "CopilotC-Nvim/CopilotChat.nvim"
+
+vim.call "plug#end"
+
+vim.cmd.colorscheme "catppuccin-mocha"
